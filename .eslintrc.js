a53ccc4a1c99df0712555ad38ebfc6e76ff6ab76
yuki96422 <nobuhara422@gmail.com>
2022-10-22 20:57:13 +0900

    eslint変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,19 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  extends: ['plugin:react/recommended', 'standard-with-typescript','airbnb-typescript'],
   overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: ['react', '@typescript-eslint', 'unused-imports'],
+  
   rules: {
     'import/prefer-default-export': 'off',
-    'dot-notation': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'error'
+    'unused-imports/no-unused-imports': 'error',
   }
 }
